2abe5519ac661f59c8fcf8ec312a5734ad415103
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2022-12-30 21:39:55 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,15 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  // let isCheck = document.getElementById('check');
+  // let isBgColor = document.getElementById('text').style.backgroundColor;
+  // if (isCheck) {
+  //   isBgColor = "#ff0000";
+  // }else {
+  //   isBgColor = '';
+  // }
+  if (document.getElementById('check')) {
+    document.getElementById('text').style.backgroundColor = "#ff0000";
+  }else {
+    document.getElementById('text').style.backgroundColor = '';
+  }
 }
